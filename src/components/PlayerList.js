4dd715b00b6be3337d1587e82de10dd698bc8afb
alexeyosipov88 <aoosipov88@gmail.com>
2022-06-10 17:02:30 -0400

    add helper funcitons

--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -1,11 +1,12 @@
 import Player from "./Player";
 import playerData from "../data/playerData";
-import matchData from "../data/matchData"
+import matchData from "../data/matchData";
+import { preparePlayerData } from "./helpers/playerHelpers";
+import { addWinsToPlayers } from "./helpers/playerHelpers";
 
 
 
 const PlayerList = () => {
-  const playersArr = Object.values(playerData);
 
   return (
 
--- src/components/helpers/playerHelpers.js
+++ src/components/helpers/playerHelpers.js
@@ -3,17 +3,17 @@ const preparePlayerData = (players) => {
 }
 
 const addWinsToPlayers = (players, matches) => {
-
-  players.forEach(element => {
+  return package.map(element => {
     const numberOfwins = 0;
     for(let i = 0; i < matches.length; i++) {
-
       if(matches[i].winner == element.gamerTag) {
         numberOfwins++;
       }
     }
     element.wins = numberOfwins;
-
+    return element;
   });
 
 }
+
+export {preparePlayerData, addWinsToPlayers};
